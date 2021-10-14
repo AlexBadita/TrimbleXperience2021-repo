38f81cf6133a34a630803442bb10de0d6418f07b
AlexBadita <baditaalexandru19@gmail.com>
2021-10-14 20:54:40 +0300

    Changing Color Button & Class Name

--- notes-app/src/app/tools/tools.component.ts
+++ notes-app/src/app/tools/tools.component.ts
@@ -24,4 +24,26 @@ export class ToolsComponent implements OnInit {
     this.titleBgColor = this.noteContent;
     this.noteContent = "";
   }
+
+  // Color changing button
+  btnColor: string = "";
+
+  changeButtonColor(): void{
+    // generate random rgb format color
+    this.btnColor = "rgb(" + this.getRandomNumber() + "," + this.getRandomNumber() + "," + this.getRandomNumber() + ")";
+  }
+
+  getRandomNumber(): number{
+    // return random number in range 0 - 255
+    return Math.floor(Math.random() * 256);
+  }
+
+  // Change class name
+  className: string = "initial-class";
+  inputName: string = "";
+
+  changeClass(): void{
+    this.className = this.inputName;
+    this.inputName = "";
+  }
 }
